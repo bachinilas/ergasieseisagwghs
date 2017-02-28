b8601aefa6cfb90df0758f2a5f8bfe13696068e6
bachinilas <nikosfraggos@hotmail.gr>
2017-02-28 15:11:13 +0200

    Add files via upload

---
+++ ergasies eisagwghs sthn episthmh twn ypologistwn.txt/ergasia7tweeter.py
@@ -0,0 +1,29 @@
+import tweepy
+from tweepy import OAuthHandler
+consumer_key = 'qTevOzd0mZGeOKhnxNE7g83xT'
+consumer_secret = 'rmE6ettU37ySbzouWTpwgwbymNtpkWPKMo7CvXQ4QxPkKtNbph'
+access_token = '836541374333857793-J4txdAqwuZXLA6bYKmcuNq6Nnnj6Lxk'
+access_secret = 'pan6POUc7rQrYIRBpTMlR5HzGWu69kCl7JJ4kSR1rXLQa'
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+api = tweepy.API(auth)
+print "Plhktrologhse ton prwto xrhsth"
+print "Plhktrologhse ton deutero xrhsth"
+i=1
+j=1
+lekseis1=0
+lekseis2=0
+for i in range(10)
+    if str=1
+    lekseis1=lekseis1+1
+for j in range(10)
+    if str=1
+    lekseis2=lekseis2+1
+
+if lekseis1>lekseis2
+   print "O prwtos xrhsths exei tis perissoteres lekseis sta teleutaia 10 tweets toy"
+elif lekseis2>lekseis1
+   print "O deuteros xrhsths exei tis perissoteres lekseis sta teleutaia 10 tweets toy"
+else
+   print "Kai oi dyo xrhstes exoyn ton idio arithmo leksewn sta teleutaia 10 tweets toys"
+   
